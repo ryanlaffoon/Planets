450d00fbb467d5e30f19d9cfd27a2b4d73e9f33c
Ryan Laffoon <ryan_laffoon@msn.com>
2017-01-01 12:50:40 -0800

    Complete overhaul, drive from data

--- action.js
+++ action.js
@@ -1,110 +1,83 @@
 (function(window, document, $, undefined){
-	var mercuryDiam,
-	venusDiam,
-	earthDiam,
-	marsDiam,
-	jupiterDiam,
-	saturnDiam,
-	uranusDiam,
-	neptuneDiam,
-	
-	mercuryDist,
-	venusDist,
-	earthDist,
-	marsDist,
-	jupiterDist,
-	saturnDist,
-	uranusDist,
-	neptuneDist,
-	
-	mercuryPeriod,
-	venusPeriod,
-	earthPeriod,
-	marsPeriod,
-	jupiterPeriod,
-	saturnPeriod,
-	uranusPeriod,
-	neptunePeriod,
-	
-	mercuryColor,
-	venusColor,
-	earthColor,
-	marsColor,
-	jupiterColor,
-	saturnColor,
-	uranusColor,
-	neptuneColor;
-	
-	mercuryDist = 16;
-	venusDist = 32;
-	earthDist = 48;
-	marsDist = 64;
-	jupiterDist = 224;
-	saturnDist = 408;
-	uranusDist = 820;
-	neptuneDist = 1284;
-	
-	mercuryDiam = 2;
-	venusDiam = 4;
-	earthDiam = 4;
-	marsDiam = 2;
-	jupiterDiam = 34;
-	saturnDiam = 30;
-	uranusDiam = 12;
-	neptuneDiam = 12;
-	
-	mercuryPeriod = 1;
-	venusPeriod = 2.554279868;
-	earthPeriod = 4.152097306;
-	marsPeriod = 7.809253154;
-	jupiterPeriod = 49.25338183;
-	saturnPeriod = 122.2655451;
-	uranusPeriod = 348.8365352;
-	neptunePeriod = 684.2108673;
-	
-	mercuryColor,
-	venusColor,
-	earthColor,
-	marsColor,
-	jupiterColor,
-	saturnColor,
-	uranusColor,
-	neptuneColor;
-	
 
-	function setOrbit(planet, period, distance){
-		$(planet + ".orbit")
-		.css("animation-duration", period + "s")
-		.css("height", distance)
-		.css("width", distance)
-		.css("margin-top", -(distance/2.0))
-		.css("margin-left", -(distance/2.0));
+    function clearInfo() {
+        $(".planetinfo").addClass("hidden");
+        $(".selectable").parent("li").addClass("passive").removeClass("active");
+    }
+
+	function setClick(planet) {
+	    $(planet + ".selectable").click(function () {
+	        $(".planetinfo").not(planet).addClass("hidden");
+	        $(".selectable").not(planet).parent("li").addClass("passive");
+	        $(".selectable").not(planet).parent("li").removeClass("active");
+	        $(planet + ".planetinfo").toggleClass("hidden");
+	        $(planet + ".selectable").parent("li").toggleClass("passive active");
+	    });
 	}
-	
-	function setPlanet(planet, diameter, color){
-		$(planet + ".planet")
+
+	function setPlanet(planet, period, distance, diameter, color) {
+	    $(planet + ".orbit")
+        .css("animation-duration", period + "s")
+        .css("height", distance)
+        .css("width", distance)
+        .css("margin-top", -(distance / 2.0))
+        .css("margin-left", -(distance / 2.0));
+
+	    $(planet + ".planet")
 		.css("height", diameter)
 		.css("width", diameter)
-		.css("margin-top", -(diameter/2.0))
-		.css("margin-left", -(diameter/2.0))
-		.css("box-shadow", "0 0 " + (diameter) + "px " + color)
-		.css("background-color", color);
+		.css("margin-top", -(diameter / 2.0))
+		.css("margin-left", -(diameter / 2.0))
+	    .css("background-color", color)
+	    .css("box-shadow", "0 0 " + (diameter / 2.0) + "px " + color);
+		setClick(planet);
+	}
+
+	function getPlanetInfo(planet) {
+	    var info =
+            "<h3>" + planet.name + "</h3>" +
+            "<div>" + planet.bio + "</div>" +
+            "<div><span class='planetinfo-label'>Radius:</span><span class='planetinfo-figure'>" + planet.radius + "</span></div>" +
+            "<div><span class='planetinfo-label'>Length of Day:</span><span class='planetinfo-figure'>" + planet.lengthOfDay + "</span></div>" +
+            "<div><span class='planetinfo-label'>Mass:</span><span class='planetinfo-figure'>" + planet.mass + "</span></div>" +
+            "<div><span class='planetinfo-label'>Orbital Period:</span><span class='planetinfo-figure'>" + planet.orbitalPeriod + "</span></div>" +
+            "<div><span class='planetinfo-label'>Gravity:</span><span class='planetinfo-figure'>" + planet.gravity + "</span></div>" +
+            "<div><span class='planetinfo-label'>Surface Area:</span><span class='planetinfo-figure'>" + planet.surfaceArea + "</span></div>";
+
+	    //var moons = [];
+
+	    $.each(planet.moons, function (key, moon) {
+	        info += ("<div>" + moon.name + "</div>");
+	    });
+
+	    //info.append(moons.join(""));
+
+	    $("<div>", {
+	        "class": "planetinfo hidden " + planet.name.toLowerCase(),
+	        html: info
+	    }).appendTo(".info");
+	}
+
+	function getMoonInfo(planet) {
+
 	}
 	
-	function setBody(planet, period, distance, diameter){
-		setOrbit(planet, period, distance);
-		setPlanet(planet, diameter);
+	function init() {
+	    $.getJSON("planets.json", function (data) {
+	        $.each(data, function (key, planet) {
+	            setPlanet(planet.selector, planet.duration, planet.widthOrbit, planet.widthPlanet, planet.color)
+	            getPlanetInfo(planet)
+	        });
+	    });
+
+	    $(".clear").click(function () {
+	        clearInfo();
+	    });
 	}
-	
+
 	$(document).ready(function() {
-		setBody(".mercury", mercuryPeriod, mercuryDist, mercuryDiam);
-		setBody(".venus", venusPeriod, venusDist, venusDiam);
-		setBody(".earth", earthPeriod, earthDist, earthDiam);
-		setBody(".mars", marsPeriod, marsDist, marsDiam);
-		setBody(".jupiter", jupiterPeriod, jupiterDist, jupiterDiam);
-		setBody(".saturn", saturnPeriod, saturnDist, saturnDiam);
-		setBody(".uranus", uranusPeriod, uranusDist, uranusDiam);
-		setBody(".neptune", neptunePeriod, neptuneDist, neptuneDiam);
+		/* Initialize */
+	    init();
 	});
 	
 })(window, document, jQuery);
